d86fee7c79037f220f41de4e7c3d3ea2f7fec6cf
Andrei Duque Lopes <andreiduquelopes@gmail.com>
2021-10-12 10:14:38 -0300

    Fixes accordingly with review

--- src/tests/v1/api/ticket/count-tickets/count-tickets.service.spec.ts
+++ src/tests/v1/api/ticket/count-tickets/count-tickets.service.spec.ts
@@ -1,7 +1,5 @@
 import { ticketMock } from "tests/mocks/ticket";
 import { countTickets } from "v1/api/ticket/count-tickets/count-tickets.service";
-import { StatusCodeEnum } from "v1/enum/status-code";
-import { CustomError } from "v1/utils/error";
 
 describe("countTickets service", () => {
 	describe("Successful", () => {
@@ -19,26 +17,6 @@ describe("countTickets service", () => {
 			}
 
 			expect(result).toStrictEqual(1);
-		});
-	});
-
-	describe("Failure", () => {
-		it("should throw a CustomError with a No ticket found message", async () => {
-			let result: any;
-
-			ticketMock.repository.findOne.mockResolvedValue(undefined);
-
-			try {
-				result = await countTickets({
-					ticketRepository: ticketMock.repository,
-				});
-			} catch (err: any) {
-				result = err;
-			}
-
-			expect(result instanceof CustomError).toBeTruthy();
-			expect(result.message).toBe("No ticket found.");
-			expect(result.statusCode).toBe(StatusCodeEnum.NOT_FOUND);
 		});
 	});
 });
--- src/v1/api/ticket/count-tickets/count-tickets.service.ts
+++ src/v1/api/ticket/count-tickets/count-tickets.service.ts
@@ -1,5 +1,3 @@
-import { StatusCodeEnum } from "v1/enum/status-code";
-import { CustomError } from "v1/utils/error";
 import { TicketRepository } from "../ticket.entity";
 
 interface Injectables {
@@ -8,10 +6,6 @@ interface Injectables {
 
 export const countTickets = async ({ ticketRepository }: Injectables) => {
 	const numberOfTickets = await ticketRepository.count();
-
-	if (!numberOfTickets) {
-		throw new CustomError("No ticket found.", StatusCodeEnum.NOT_FOUND);
-	}
 
 	return numberOfTickets;
 };
